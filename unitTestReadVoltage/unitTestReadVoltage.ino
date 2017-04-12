0af119fd97b673d25df97b45da494d126a8d4f45
Leila Huang <leilahuang95@gmail.com>
2017-04-12 23:45:46 +0800

    succeedReadVoltage
    it can detect the voltage of each resistance

--- unitTestReadVoltage/unitTestReadVoltage.ino
+++ unitTestReadVoltage/unitTestReadVoltage.ino
@@ -5,9 +5,11 @@ void setup() {
 
 void loop() {
   // read the input on analog pin 0:
-  int sensorValue = analogRead(A0);
-  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+  int sensorValue = analogRead(A5);
+//  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
   float voltage = sensorValue * (5.0 / 1023.0);
   // print out the value you read:
   Serial.println(voltage);
+  //Serial.println(sensorValue);
 }
+

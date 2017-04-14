d5a84af2b40daa0adc066b1ee328601c136c6cc8
Leila Huang <leilahuang95@gmail.com>
2017-04-14 20:57:10 +0800

    蓝牙连接代码

--- unitTestReadVoltage/unitTestReadVoltage.ino
+++ unitTestReadVoltage/unitTestReadVoltage.ino
@@ -9,7 +9,18 @@ void loop() {
 //  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
   float voltage = sensorValue * (5.0 / 1023.0);
   // print out the value you read:
-  Serial.println(voltage);
+ // Serial.println(voltage);
   //Serial.println(sensorValue);
+
+ if(voltage>3.3&& voltage<3.4){
+   Serial.println("module 2 detected ");
+ }
+ else if (voltage>4.7&& voltage<4.8){
+   Serial.println("module 1 detected ");
+ }
+ else{
+   Serial.println("no module detected ");
+ }
+
 }
 
